cd9a69a68ae8d90dee64799c98bfa437ba9d1a02
Ritesh <riteshsahu1981@gmail.com>
2021-10-19 01:16:08 +0530

    changes done

--- autologin.php
+++ autologin.php
@@ -1,44 +1,24 @@
 <?php
-
 include( 'config.php' );
+require('user/lib.php');
 
-/* if(isloggedin()){
-	echo "Logout action";
-	require_logout();
-} */
+$payload=json_decode(base64_decode($_GET['enc_pl']), true);
 
-$username=$_GET['username'];
+$username=$payload['username'];
 $password='fredpass6';
-$firstname=$_GET['firstname'];
-$lastname=$_GET['lastname'];
-$email=$_GET['email'];
+$firstname=$payload['firstname'];
+$lastname=$payload['lastname'];
+$email=$payload['email'];
 
 $user = $DB->get_record('user', array('username' => $username));
+
 if($user===false){
 	$user=create_user_record( $username,$password );
-}else{
-	echo "User exists ". $user->id;
 }
+
 $user->firstname=$firstname;
 $user->lastname=$lastname;
 $user->email=$email;
-require('user/lib.php');
 user_update_user($user);
 complete_user_login($user);
 redirect('/');
-	
-//
-//var_dump(validate_internal_user_password($username,$password));
-
-/* if($user=authenticate_user_login($username, $password, true))
-{
-	$user->firstname=$firstname;
-	$user->lastname=$lastname;
-	$user->email=$email;
-	require('user/lib.php');
-	user_update_user($user);
-	complete_user_login($user);
-	
-	
-} */
-//var_dump($user);
